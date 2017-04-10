9f4cfc848275eb53210287515aa0a7511887020a
Tim <Tim.mouse@gmail.com>
2017-04-11 07:54:32 +0800

    change api source

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,7 +15,8 @@ export class HeroService {
   }
   private extractData(res: Response) {
     let body = res.json();
-    return body.data || { };
+    return body.Users || { };
+    
   }
   private handleError (error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
